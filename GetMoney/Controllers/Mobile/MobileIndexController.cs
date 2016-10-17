f87e7c2308cb7fe37117605043a09fce3a0ac513
秋龙翔天 <a79361360@163.com>
2016-10-18 02:03:36 +0800

    123

---
+++ GetMoney/Controllers/Mobile/MobileIndexController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetMoney.Controllers.Mobile
+{
+    public class MobileIndexController : Controller
+    {
+        //
+        // GET: /MobileIndex/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
