9cfeddef80255d6702a22cac0bb7103751ec388a
秋龙翔天 <a79361360@163.com>
2016-11-30 00:42:57 +0800

    123

---
+++ GetMoney/Controllers/OnlyNameTest/OnlyNameTestController.cs
@@ -0,0 +1,35 @@
+﻿using GetMoney.Application.OnlyNameTest;
+using GetMoney.Framework;
+using GetMoney.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetMoney.Controllers.OnlyNameTest
+{
+    public class OnlyNameTestController : BaseController
+    {
+        //
+        // GET: /OnlyNameTest/
+        IOnlyNameTestBll _bll = null;
+        public OnlyNameTestController(IOnlyNameTestBll bll)
+        {
+            _bll = bll;
+        }
+        public void TestDbcontext()
+        {
+            OnlyNameTestDto dto = new OnlyNameTestDto();
+            //新增
+            dto.Name = "qiuqiu";
+            dto.InputDate = DateTime.Now;
+            _bll.Add(dto);
+            //Db.OnlyNameTest.Add(dto);
+            //Db.SaveChanges();
+
+        }
+    }
+}
