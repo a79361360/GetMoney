d24f36e0217da582e1dbca59e1eeab9e2dcbc2d7
秋龙翔天 <a79361360@163.com>
2016-12-01 02:07:37 +0800

    234

--- GetMoney/Controllers/OnlyNameTest/OnlyNameTestController.cs
+++ GetMoney/Controllers/OnlyNameTest/OnlyNameTestController.cs
@@ -34,5 +34,38 @@ namespace GetMoney.Controllers.OnlyNameTest
         public ActionResult Test() {
             return View();
         }
+        public ActionResult TTest()
+        {
+            Get_img();
+            return View();
+        }
+        public Bitmap Get_img()
+        {
+            Bitmap img = null;
+            HttpWebRequest req;
+            HttpWebResponse res = null;
+            try
+            {
+                System.Uri httpUrl = new System.Uri("http://img.my.csdn.net/uploads/201504/28/1430184401_2155.jpg");
+                req = (HttpWebRequest)(WebRequest.Create(httpUrl));
+                req.Timeout = 180000; //设置超时值10秒
+                //req.UserAgent = "Mozilla/4.0";
+                //req.Accept = "XXXXXX";
+                req.Method = "GET";
+                res = (HttpWebResponse)(req.GetResponse());
+                img = new Bitmap(res.GetResponseStream());//获取图片流                
+                img.Save(@"E:/" + DateTime.Now.ToFileTime().ToString() + ".jpg");//随机名
+            }
+
+            catch (Exception ex)
+            {
+                string aa = ex.Message;
+            }
+            finally
+            {
+                res.Close();
+            }
+            return img;
+        }
     }
 }
